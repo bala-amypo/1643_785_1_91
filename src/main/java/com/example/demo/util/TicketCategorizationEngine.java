9c8400114b29bc63f7389a4239e27a75da9253ee
bala-amypo <bala@amypo.in>
2025-12-18 15:38:25 +0000

    User commit

--- src/main/java/com/example/demo/util/TicketCategorizationEngine.java
+++ src/main/java/com/example/demo/util/TicketCategorizationEngine.java
@@ -5,6 +5,17 @@ import com.example.demo.model.*;
 
 public class TicketCategorizationEngine{
     public void categorization(
-        Ticket
-    )
+        Ticket t,
+        List<Category> cats,
+        List<CategorizationRule> rules,
+        List<UrgencyPolicy> policies,
+        List<CategorizationLog> logs
+    ){
+        String text = (t.getTitle() + " " + t.getDescription()).toLowerCase();
+        rules.stream()
+                .sorted(Comparator.comparing(CategorizationRule::getPriority).reversed())
+                .forEach(rule -> {
+                    if(text.c)
+                })
+    }
 }
