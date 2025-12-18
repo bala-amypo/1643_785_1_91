1db43a867ac316e40e0a8e805e371bda1b83fe14
bala-amypo <bala@amypo.in>
2025-12-18 15:29:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/TicketService.java
@@ -0,0 +1,3 @@
+public interface TicketService{
+    Ticket create
+}
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -9,5 +9,15 @@ public class YserServiceImpl implements UserService{
     public User register(User user){
         if(repo.existsByEmail(user.getEmail()))
             throw new RuntimeException("Email already in use");
+        user.setPasssword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+    public UserGetUser(Long id){
+        return repo.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+    public User findByEmail(String email){
+        return repo.findByEmail(email);
+    }
     
 }
