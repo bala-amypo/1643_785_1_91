4ece3021f015ab717900b947011958d4fd9174f8
bala-amypo <bala@amypo.in>
2025-12-18 14:50:25 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HealthServlet.java
+++ src/main/java/com/example/demo/servlet/HealthServlet.java
@@ -1,5 +1,10 @@
 package com.example.demo.servlet;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+
+
+
 @WebSer
 public class HealthServlet extends HealthServlet {
 
