b64a503ecf1edbc1847d21c259cba77ef8e9bb46
bala-amypo <bala@amypo.in>
2025-12-19 14:09:21 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,17 +1,1 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-
-@Entity
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-}
+\
