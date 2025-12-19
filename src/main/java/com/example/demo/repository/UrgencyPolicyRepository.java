1278be5d189c083b0debf538b38059e6c90fedf1
bala-amypo <bala@amypo.in>
2025-12-19 14:12:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/UrgencyPolicyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.UrgencyPolicy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrgencyPolicyRepository
-        extends JpaRepository<UrgencyPolicy, Long> {
-}
