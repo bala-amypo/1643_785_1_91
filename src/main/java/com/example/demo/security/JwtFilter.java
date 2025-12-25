220bd8e42b2c58d45696cd12fcbbf9802ec8346c
bala-amypo <bala@amypo.in>
2025-12-25 13:48:39 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -25,6 +25,12 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/api/auth/");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
